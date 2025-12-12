8dce1bfcc89a2cff5450ae780caac3e2db73230e
gosha <88@.ru>
2025-12-12 14:45:00 +0500

    add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.burger.close');
 const headerTopListMobile = document.querySelector('.header__top-list--mobile');
 const headerNav = document.querySelector('.header__nav');
 const headerWrapperMobile = document.querySelector('.header__wrapper--mobile');
@@ -6,7 +7,10 @@ const headerWrapperMobile = document.querySelector('.header__wrapper--mobile');
 burger.addEventListener('click', () => {
     const body = document.body;
 
-    headerWrapperMobile.classList.toggle('active');
-    burger.classList.toggle('active');
+    headerWrapperMobile.classList.add('active');
     body.classList.toggle('stop-scroll');
+})
+
+burgerClose.addEventListener('click', () => {
+    headerWrapperMobile.classList.remove('active');
 })
